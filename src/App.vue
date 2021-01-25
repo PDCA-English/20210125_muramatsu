a038e3669756897767e5bf196a4a6019a532bd0a
Takuya <r28.6sa@gmail.com>
2021-01-25 23:06:08 +0900

    first-attempt

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,34 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+      <input type="text" v-model="post_code">
+      <button @click="checkPostCode">住所自動入力</button>
+      <br>
+      <br>
+      <div>Address：{{ post_info.allAddress }}</div>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      api_key: '6jBX9cqI2JCM7XsTMssELr4P1PrLnkrHQDBiE0v',
+      url_base: 'https://apis.postcode-jp.com/api/v2/postcodes/',
+      post_code: '',
+      post_info: {}
+    }
+  },
+  methods: {
+    checkPostCode () {
+        fetch(`${this.url_base}${this.post_code}?apiKey=${this.api_key}`)
+          .then(res => {
+            return res.json();
+          }).then(this.setResults);
+    },
+    setResults (results) {
+      this.post_info = results;
+    }
+  }
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
